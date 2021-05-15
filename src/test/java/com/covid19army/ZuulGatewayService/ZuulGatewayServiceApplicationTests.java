16fd01f006731db05b3aa9d234da9cb6d3eb667e
Ravi Kumar K G <ravi.rk2406@gmail.com>
2021-05-15 10:34:17 +0000

    Zuul gateway service initial code commit

---
+++ src/test/java/com/covid19army/ZuulGatewayService/ZuulGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covid19army.ZuulGatewayService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

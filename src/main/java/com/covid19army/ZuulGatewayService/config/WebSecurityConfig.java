b9e691c22b5957d62a17c140600b13c47cce5dde
ravir_000 <ravi.rk2406@gmail.com>
2021-07-19 17:35:07 +0530

    enabled session api

--- src/main/java/com/covid19army/ZuulGatewayService/config/WebSecurityConfig.java
+++ src/main/java/com/covid19army/ZuulGatewayService/config/WebSecurityConfig.java
@@ -49,7 +49,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		httpSecurity.csrf().disable()
 				// dont authenticate this particular request
 				.authorizeRequests().antMatchers("/**/validateotp", 
-						"/**/login","/**/health").permitAll().
+						"/**/login","/**/health,/**/session").permitAll().
 				// all other requests need to be authenticated
 				anyRequest().authenticated().and().
 				// make sure we use stateless session; session won't be used to
